8ec0244f46e281688a438bab23978fc10f753033
sukyong yoon <coolnice@msn.com>
2022-05-17 18:22:38 -0700

    Rollback sever/db/db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://dev:7ujm8ik,@192.168.140.130:5432/messenger", {
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
   logging: false
 });
 
